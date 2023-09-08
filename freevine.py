8ca94d8d5090ce213bac6169120dd0102b9c7bd0
stabbedbybrick <stabbedbybrick@protonmail.com>
2023-09-08 19:26:22 +0200

    renamed main script

---
+++ freevine.py
@@ -0,0 +1,38 @@
+import shutil
+
+from pathlib import Path
+
+import click
+import yaml
+
+from helpers import __version__
+from helpers.documentation import main_help
+from helpers.alias import get_alias
+from helpers.utilities import stamp
+
+
+@click.command(help=main_help)
+@click.argument("url", type=str, required=True)
+@click.option("-q", "--quality", type=str, help="Specify resolution")
+@click.option("-a", "--all-audio", is_flag=True, help="Include all audio tracks")
+@click.option("-e", "--episode", type=str, help="Download episode(s)")
+@click.option("-s", "--season", type=str, help="Download complete season")
+@click.option("-c", "--complete", is_flag=True, help="Download complete series")
+@click.option("-m", "--movie", is_flag=True, help="Download movie")
+@click.option("-t", "--titles", is_flag=True, default=False, help="List all titles")
+@click.option("-r", "--remote", is_flag=True, default=False, help="Use remote CDM")
+def main(**kwargs) -> None:
+    click.echo("")
+    stamp(f"Freevine v{__version__}\n")
+
+    with open("config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    Service = get_alias(kwargs.get("url"))
+    Service(config, **kwargs)
+
+    shutil.rmtree("tmp") if Path("tmp").exists() else None
+
+
+if __name__ == "__main__":
+    main()
