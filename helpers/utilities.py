d9d919a862e85331f437c3e8145c1d54563f739a
stabbedbybrick <stabbedbybrick@protonmail.com>
2023-09-18 07:54:36 +0200

    add character to string cleaning

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -23,7 +23,7 @@ def error(text: str) -> str:
 
 def string_cleaning(filename: str) -> str:
     filename = filename.replace("&", "and")
-    filename = re.sub(r"[:; ]", ".", filename)
+    filename = re.sub(r"[:;/ ]", ".", filename)
     filename = re.sub(r"[\\*!?¿,'\"()<>|$#`]", "", filename)
     filename = re.sub(rf"[{'.'}]{{2,}}", ".", filename)
     return filename
