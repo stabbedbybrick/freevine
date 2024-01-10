3b1d85cab4e9f9926d6b5cdf3e825d3320527e30
stabbedbybrick <125766685+stabbedbybrick@users.noreply.github.com>
2024-01-10 19:38:48 +0100

    [cbc] Add requirement for login credentials (#57)

--- services/cbc/cbc.py
+++ services/cbc/cbc.py
@@ -9,13 +9,17 @@ Quality: up to 1080p and DDP5.1 audio
 """
 from __future__ import annotations
 
+import json
 import re
 import subprocess
 import sys
 from collections import Counter
+from datetime import datetime, timedelta
+from pathlib import Path
 from urllib.parse import urlparse
 
 import click
+import yaml
 from bs4 import BeautifulSoup
 
 from utils.args import get_args
@@ -23,11 +27,11 @@ from utils.config import Config
 from utils.options import get_downloads
 from utils.titles import Episode, Movie, Movies, Series
 from utils.utilities import (
+    force_numbering,
     is_url,
     set_filename,
     set_save_path,
     string_cleaning,
-    force_numbering,
 )
 
 
@@ -43,28 +47,61 @@ class CBC(Config):
             self.log.info("Subtitle downloads are not supported on this service")
             return
 
+        self.username = self.config.get("credentials", {}).get("username")
+        self.password = self.config.get("credentials", {}).get("password")
+
         self.get_options()
 
-    def get_claims_token(self):
+    def login(self) -> str:
+        if not self.username and not self.password:
+            self.log.error(
+                "Required credentials were not found. See 'freevine.py profile --help'"
+            )
+            sys.exit(1)
+
         payload = {
-            "email": self.config["email"],
-            "password": self.config["password"],
+            "email": self.username,
+            "password": self.password,
         }
-        headers = {"content-type": "application/json"}
         params = {"apikey": self.config["apikey"]}
-        resp = self.client.post(
+        r = self.client.post(
             "https://api.loginradius.com/identity/v2/auth/login",
             json=payload,
             params=params,
-        ).json()
+        )
+        if not r.ok:
+            raise ConnectionError(f"{r.json().get('Description')}")
 
-        access_token = resp["access_token"]
+        auth = json.loads(r.content)
+        access_token = auth.get("access_token")
+        refresh_token = auth.get("refresh_token")
+        expiry = datetime.strptime(auth.get("expires_in"), "%Y-%m-%dT%H:%M:%S.%fZ")
 
+        profile = Path("services") / "cbc" / "profile.yaml"
+        with open(profile, "r") as f:
+            data = yaml.safe_load(f)
+
+        data["cache"] = {}
+
+        data["cache"]["login"] = {
+            "token": access_token,
+            "expiry": expiry,
+            "refresh": refresh_token,
+        }
+
+        with open(profile, "w") as f:
+            yaml.safe_dump(data, f, sort_keys=False)
+
+        self.log.info("+ New login tokens placed in cache")
+        return access_token
+
+    def access_token(self, token: str) -> str:
         params = {
-            "access_token": access_token,
+            "access_token": token,
             "apikey": self.config["apikey"],
             "jwtapp": "jwt",
         }
+        headers = {"content-type": "application/json"}
         resp = self.client.get(
             "https://cloud-api.loginradius.com/sso/jwt/api/token",
             headers=headers,
@@ -75,18 +112,38 @@ class CBC(Config):
 
         payload = {"jwt": sig}
         headers = {"content-type": "application/json", "ott-device-type": "web"}
-        resp = self.client.post(
+        auth = self.client.post(
             "https://services.radio-canada.ca/ott/cbc-api/v2/token",
             headers=headers,
             json=payload,
         ).json()
 
-        cbc_access_token = resp["accessToken"]
+        access_token = auth.get("accessToken")
+        refresh_token = auth.get("refreshToken")
 
+        expiry = datetime.now() + timedelta(seconds=auth.get("accessTokenExpiresIn"))
+
+        profile = Path("services") / "cbc" / "profile.yaml"
+        with open(profile, "r") as f:
+            data = yaml.safe_load(f)
+
+        data["cache"]["access"] = {
+            "token": access_token,
+            "expiry": expiry,
+            "refresh": refresh_token,
+        }
+
+        with open(profile, "w") as f:
+            yaml.safe_dump(data, f, sort_keys=False)
+
+        self.log.info("+ New access tokens placed in cache")
+        return access_token
+
+    def claims_token(self, token: str) -> str:
         headers = {
             "content-type": "application/json",
             "ott-device-type": "web",
-            "ott-access-token": cbc_access_token,
+            "ott-access-token": token,
         }
         resp = self.client.get(
             "https://services.radio-canada.ca/ott/cbc-api/v2/profile",
@@ -95,10 +152,69 @@ class CBC(Config):
 
         return resp["claimsToken"]
 
+    def refresh_auth_token(self, token: str) -> str:
+        payload = {
+            "email": self.username,
+            "password": self.password,
+            "refresh_token": token,
+        }
+        params = {"apikey": self.config["apikey"]}
+        auth = self.client.post(
+            "https://api.loginradius.com/identity/v2/auth/login",
+            json=payload,
+            params=params,
+        )
+        
+        access_token = auth.get("access_token")
+        refresh_token = auth.get("refresh_token")
+        expiry = datetime.strptime(auth.get("expires_in"), "%Y-%m-%dT%H:%M:%S.%fZ")
+
+        profile = Path("services") / "cbc" / "profile.yaml"
+        with open(profile, "r") as f:
+            data = yaml.safe_load(f)
+
+        data["cache"]["login"]["token"] = access_token
+        data["cache"]["login"]["refresh"] = refresh_token
+        data["cache"]["login"]["expiry"] = expiry
+
+        with open(profile, "w") as f:
+            yaml.safe_dump(data, f, sort_keys=False)
+
+        self.log.info("+ Tokens refreshed")
+        return access_token
+
+    def authenticate(self):
+        cache = self.config.get("cache")
+
+        if not cache:
+            self.log.info("Cache is empty, aquiring new login tokens...")
+            auth_token = self.login()
+            access_token = self.access_token(auth_token)
+
+        elif cache and cache["login"]["expiry"] < datetime.now():
+            self.log.info("Refreshing expired login tokens...")
+            auth_token = self.refresh_auth_token(cache["login"]["refresh"])
+
+        else:
+            self.log.info("Using cached login tokens")
+            auth_token = cache["login"].get("token")
+
+        if not cache and access_token:
+            access_token = access_token
+
+        elif cache and cache["access"]["expiry"] < datetime.now():
+            access_token = self.access_token(auth_token)
+
+        else:
+            access_token = cache["access"]["token"]
+
+        return access_token
+
     def get_data(self, url: str):
         show_id = urlparse(url).path.split("/")[1]
 
-        claims_token = self.get_claims_token()
+        access_token = self.authenticate()
+        claims_token = self.claims_token(access_token)
         self.client.headers.update({"x-claims-token": claims_token})
         url = self.config["shows"].format(show=show_id)
 
@@ -225,7 +341,7 @@ class CBC(Config):
                     for level in index:
                         if not level.attrs.get("Bitrate"):
                             continue
-                        
+
                         if level.attrs.get("MaxHeight") == "1080":
                             m3u8_text += (
                                 video_stream.format(
@@ -269,29 +385,27 @@ class CBC(Config):
         response = self.client.get(playsession).json()
 
         if response.get("errorCode"):
-             raise ConnectionError(response)
+            raise ConnectionError(response)
 
         return self.get_hls(response.get("url"))
 
     def get_content(self, url: str) -> object:
         if self.movie:
-            with self.console.status("Fetching movie titles..."):
-                content = self.get_movies(self.url)
-                title = string_cleaning(str(content))
+            content = self.get_movies(self.url)
+            title = string_cleaning(str(content))
 
             self.log.info(f"{str(content)}\n")
 
         else:
-            with self.console.status("Fetching series titles..."):
-                content = self.get_series(url)
+            content = self.get_series(url)
 
-                title = string_cleaning(str(content))
-                seasons = Counter(x.season for x in content)
-                num_seasons = len(seasons)
-                num_episodes = sum(seasons.values())
+            title = string_cleaning(str(content))
+            seasons = Counter(x.season for x in content)
+            num_seasons = len(seasons)
+            num_episodes = sum(seasons.values())
 
-                if self.force_numbering:
-                    content = force_numbering(content)
+            if self.force_numbering:
+                content = force_numbering(content)
 
             self.log.info(
                 f"{str(content)}: {num_seasons} Season(s), {num_episodes} Episode(s)\n"
